fa716990f80ef3a008e4f337cd71c563a5d56b04
maarklam <maarklam@gmail.com>
2017-06-06 17:20:34 -0400

    Add files via upload

--- magic_carpet.js
+++ magic_carpet.js
@@ -45,7 +45,10 @@ Blockly.Blocks['load_main'] = {
 	  this.removeInput('LOAD_STRING7');
 	  this.appendValueInput("LOAD_STRING")
 	  .setCheck(null)
-	  .appendField("Path:");      
+	  .appendField("Path:");	  
+	  this.appendDummyInput("LOAD_STRING2")	  
+	  .appendField("Time Series Data:")
+      .appendField(new Blockly.FieldDropdown([["Y","TIMESERIES_YES"], ["N","TIMESERIES_NO"]]), "TIMESERIES");
     }
 	else if (loadInput=="FROM_GP") {
       this.removeInput('LOAD_STRING');
@@ -115,6 +118,9 @@ Blockly.Blocks['load_main'] = {
 	  this.appendValueInput("LOAD_STRING2")
 	  .setCheck(null)
 	  .appendField("Metric(s):");	  
+	  this.appendValueInput("LOAD_STRING3")
+	  .setCheck(null)
+	  .appendField("Price:");	  	  
 	  this.appendDummyInput("LOAD_STRING4")
 	  .appendField("Date:")
 	  .appendField(new Blockly.FieldDate(), 'STRING4_DATE');	  
@@ -223,7 +229,7 @@ Blockly.Blocks['visualize_main'] = {
 Blockly.Blocks['transform_main'] = {
   init: function() {
 	var PROPERTIES =
-	[["Filter","TRANSFORM_FILTER"], ["New Column","TRANSFORM_NEWCOL"], ["Rename Column","TRANSFORM_RENAME"],["Dates","TRANSFORM_DATES"],["Update Column","TRANSFORM_UPDATE"]]; 
+	[["Filter","TRANSFORM_FILTER"], ["New Column","TRANSFORM_NEWCOL"], ["Rename Column","TRANSFORM_RENAME"],["Dates","TRANSFORM_DATES"],["Update Column","TRANSFORM_UPDATE"],["Concatenate","TRANSFORM_CONCAT"],["Index","TRANSFORM_INDEX"]]; 
 	var dropdown = new Blockly.FieldDropdown(PROPERTIES, function(option) {
 	  var loadInput = option;
 	  this.sourceBlock_.updateShape_(loadInput);
@@ -297,6 +303,28 @@ Blockly.Blocks['transform_main'] = {
 	  .setCheck(null)
 	  .appendField("Rename To:");      
     }
+	else if (loadInput=="TRANSFORM_CONCAT") {
+      this.removeInput('LOAD_STRING');
+	  this.removeInput('LOAD_COL');
+	  this.removeInput('LOAD_CONDITION');
+	  this.appendValueInput("LOAD_STRING")
+	  .setCheck(null)
+	  .appendField("Dataframe 1:");      
+	  this.appendValueInput("LOAD_COL")
+	  .setCheck(null)
+	  .appendField("Dataframe 2:");      	  
+    }
+    else if (loadInput=="TRANSFORM_INDEX") {
+      this.removeInput('LOAD_STRING');
+	  this.removeInput('LOAD_COL');
+	  this.removeInput('LOAD_CONDITION');
+	  this.appendValueInput("LOAD_STRING")
+	  .setCheck(null)
+	  .appendField("Using Data:");      
+	  this.appendValueInput("LOAD_COL")
+	  .setCheck(null)
+	  .appendField("Column:");      
+	}	
 	else {
 		this.removeInput('LOAD_STRING');
 		this.removeInput('LOAD_COL');
@@ -308,7 +336,7 @@ Blockly.Blocks['transform_main'] = {
 Blockly.Blocks['aggregate_main'] = {
   init: function() {
 	var PROPERTIES =
-	[["Sum","AGGREGATE_SUM"], ["Average","AGGREGATE_AVG"], ["Weighted Average","AGGREGATE_WTDAVG"],["Merge","AGGREGATE_MERGE"]]; 
+	[["Sum","AGGREGATE_SUM"], ["Average","AGGREGATE_AVG"], ["Weighted Average","AGGREGATE_WTDAVG"],["Merge","AGGREGATE_MERGE"],["Min","AGGREGATE_MIN"],["Max","AGGREGATE_MAX"]]; 
 	var dropdown = new Blockly.FieldDropdown(PROPERTIES, function(option) {
 	  var loadInput = option;
 	  this.sourceBlock_.updateShape_(loadInput);
@@ -388,6 +416,18 @@ Blockly.Blocks['aggregate_main'] = {
 	  .setCheck(null)
 	  .appendField("Weighted By:");      
 	}
+	else if (loadInput=="AGGREGATE_MIN" || loadInput=="AGGREGATE_MAX") {
+		this.removeInput('LOAD_STRING');
+	  this.removeInput('LOAD_COL');
+	  this.removeInput('LOAD_CONDITION');
+	  this.removeInput('LOAD_CONDITION2');
+	  this.appendValueInput("LOAD_STRING")
+	  .setCheck(null)
+	  .appendField("From Data:");      
+	  this.appendValueInput("LOAD_COL")
+	  .setCheck(null)
+	  .appendField("Column:");	  
+	}
 	else {      
 		this.removeInput('LOAD_STRING');
 		this.removeInput('LOAD_COL');
@@ -426,44 +466,48 @@ Blockly.Blocks['fit_main'] = {
   init: function() {
 	this.appendDummyInput()
 		.appendField(new Blockly.FieldDropdown([["Regression","FIT_REGRESSION"], ["Random Forest","FIT_FOREST"]]), "FIT_TYPE");
+	this.appendDummyInput()
+	  .appendField("Return")
+      .appendField(new Blockly.FieldDropdown([["All Output","RETURN_ALL"], ["Coefficient(s)","RETURN_COEF"], ["R2","RETURN_R2"], ["Plot","RETURN_PLOT"]]), "RETURN_TYPE");
 	this.appendValueInput("LOAD_STRING")
 	  .setCheck(null)
 	  .appendField("Using Data:");
-	this.appendValueInput("LOAD_RATIO")
+	/*this.appendValueInput("LOAD_RATIO")
 	  .setCheck(null)
-	  .appendField("Training Using Ratio:");
-	this.appendDummyInput('LOAD_DUMMY')
+	  .appendField("Training Using Ratio:");*/
+	this.appendDummyInput()
 	  .appendField("");
 	this.appendValueInput("LOAD_DEPVAR")
 	  .setCheck(null)
 	  .appendField("Dependent Variable:");      
-	this.appendDummyInput('LOAD_DUMMY2')
+	this.appendDummyInput()
 	  .setAlign(Blockly.ALIGN_CENTRE)
 	  .appendField("On");
 	this.appendValueInput("LOAD_INDEPVAR")
 	  .setCheck(null)
-	  .appendField("Independent Variable(s):");      	  
+	  .appendField("Independent Variable(s):");    
 	this.setInputsInline(false);
     this.setColour('#fcd015');
     this.setTooltip('');
     this.setHelpUrl('');
 	this.setMutator(new Blockly.Mutator(['fit_newvariable']));
-	this.elseifCount_ = 0;
+	this.variableCount_ = 0;
+	this.setPreviousStatement(true, null);	
   },
   
     mutationToDom: function() {
-    if (!this.elseifCount_) {
+    if (!this.variableCount_) {
       return null;
     }
     var container = document.createElement('mutation');
-    if (this.elseifCount_) {
-      container.setAttribute('elseif', this.elseifCount_);
+    if (this.variableCount_) {
+      container.setAttribute('vars', this.variableCount_);
     }
     return container;
   },
   
    domToMutation: function(xmlElement) {
-    this.elseifCount_ = parseInt(xmlElement.getAttribute('elseif'), 10) || 0;
+    this.variableCount_ = parseInt(xmlElement.getAttribute('vars'), 10) || 0;
     this.updateShape_();
   },
   
@@ -471,7 +515,7 @@ Blockly.Blocks['fit_main'] = {
     var containerBlock = workspace.newBlock('fit_variable');
     containerBlock.initSvg();
     var connection = containerBlock.nextConnection;
-    for (var i = 1; i <= this.elseifCount_; i++) {
+    for (var i = 1; i <= this.variableCount_; i++) {
       var elseifBlock = workspace.newBlock('fit_newvariable');
       elseifBlock.initSvg();
       connection.connect(elseifBlock.previousConnection);
@@ -483,14 +527,14 @@ Blockly.Blocks['fit_main'] = {
    compose: function(containerBlock) {
     var clauseBlock = containerBlock.nextConnection.targetBlock();
     // Count number of inputs.
-    this.elseifCount_ = 0;
+    this.variableCount_ = 0;
     var valueConnections = [null];
     var statementConnections = [null];
-    var elseStatementConnection = null;
+    
     while (clauseBlock) {
       switch (clauseBlock.type) {
         case 'fit_newvariable':
-          this.elseifCount_++;
+          this.variableCount_++;
           valueConnections.push(clauseBlock.valueConnection_);
           statementConnections.push(clauseBlock.statementConnection_);
           break;
@@ -502,10 +546,10 @@ Blockly.Blocks['fit_main'] = {
     }
     this.updateShape_();
     // Reconnect any child blocks.
-    for (var i = 1; i <= this.elseifCount_; i++) {
-      Blockly.Mutator.reconnect(valueConnections[i], this, 'IF' + i);
+    for (var i = 1; i <= this.variableCount_; i++) {
+      Blockly.Mutator.reconnect(valueConnections[i], this, 'VAR' + i);
     }
-    Blockly.Mutator.reconnect(elseStatementConnection, this, 'ELSE');
+    
   },
   
   saveConnections: function(containerBlock) {
@@ -514,18 +558,14 @@ Blockly.Blocks['fit_main'] = {
     while (clauseBlock) {
       switch (clauseBlock.type) {
         case 'fit_newvariable':
-          var inputIf = this.getInput('IF' + i);
+          var inputIf = this.getInput('VAR' + i);
           clauseBlock.valueConnection_ =
               inputIf && inputIf.connection.targetConnection;
           clauseBlock.statementConnection_ =
               inputDo && inputDo.connection.targetConnection;
           i++;
           break;
-        case 'controls_if_else':
-          var inputDo = this.getInput('ELSE');
-          clauseBlock.statementConnection_ =
-              inputDo && inputDo.connection.targetConnection;
-          break;
+
         default:
           throw 'Unknown block type.';
       }
@@ -535,18 +575,15 @@ Blockly.Blocks['fit_main'] = {
   },
   
   updateShape_: function() {
-    // Delete everything.
-    if (this.getInput('ELSE')) {
-      this.removeInput('ELSE');
-    }
+    // Delete everything.    
     var i = 1;
-    while (this.getInput('IF' + i)) {
-      this.removeInput('IF' + i);
+    while (this.getInput('VAR' + i)) {
+      this.removeInput('VAR' + i);
       i++;
     }
     // Rebuild block.
-    for (var i = 1; i <= this.elseifCount_; i++) {
-      this.appendValueInput('IF' + i);
+    for (var i = 1; i <= this.variableCount_; i++) {
+      this.appendValueInput('VAR' + i);
     }
   }
 };
@@ -556,28 +593,40 @@ Blockly.Python['load_main'] = function(block) {
   var string_input = Blockly.Python.valueToCode(block, 'LOAD_STRING', Blockly.Python.ORDER_ATOMIC);
   var string2_input = Blockly.Python.valueToCode(block, 'LOAD_STRING2', Blockly.Python.ORDER_ATOMIC);
   var string3_input = Blockly.Python.valueToCode(block, 'LOAD_STRING3', Blockly.Python.ORDER_ATOMIC);
-  var string4_input = block.getFieldValue('STRING4_DATE');
+  var string4_input = block.getFieldValue('STRING4_DATE'); //String4 is reserved for dates
   var string5_input = Blockly.Python.valueToCode(block, 'LOAD_STRING5', Blockly.Python.ORDER_ATOMIC);
   var string6_input = Blockly.Python.valueToCode(block, 'LOAD_STRING6', Blockly.Python.ORDER_ATOMIC);
   var string7_input = Blockly.Python.valueToCode(block, 'LOAD_STRING7', Blockly.Python.ORDER_ATOMIC);
   
   var loadInput = this.getFieldValue('PROPERTY'); 
+  var timeseries_input = this.getFieldValue('TIMESERIES');
   
+  string_input = string_input.replace(/[']/g,""); //Remove quotes from string
+  string2_input = string2_input.replace(/[']/g,""); //Remove quotes from string
+  string3_input = string3_input.replace(/[']/g,""); //Remove quotes from string
+  string5_input = string5_input.replace(/[']/g,""); //Remove quotes from string
+  string6_input = string6_input.replace(/[']/g,""); //Remove quotes from string
+  string7_input = string7_input.replace(/[']/g,""); //Remove quotes from string  
+    
   // TODO: Assemble into code variable.
-  if (loadInput=="FROM_FILE") {	
-	string_input = string_input.replace(/[']/g,""); //Remove quotes from string
+  if (loadInput=="FROM_FILE") {
+	string_input = string_input.replace(/["]/g,""); //Remove double quotes from string. This is an abnormal use case where a string needs to be passed in without quotes, due to the "file:///" syntax required.
+	if (timeseries_input=="TIMESERIES_NO") {
 	var code = 'pd.read_csv("file:///' + string_input + '")\n';	
+	}
+	else {
+	var code = 'pd.read_csv("file:///' + string_input + '", index_col=0, parse_dates=True, infer_datetime_format=True)\n';	
+	}
   }
-  else if (loadInput=="FROM_GP") {
-	string_input = string_input.replace(/[']/g,""); //Remove quotes from string
+  else if (loadInput=="FROM_GP") {	
 	var code = 'ATP.get_data("aladdin.gp", url="' + string_input + '?")\n';
   }
   else if (loadInput=="FROM_ANSER") {
-	var code = 'ATP.get_data("aladdin.anser", **{"pricer": {"asset_id": ' + string_input + '}, "metrics": [' + string2_input + ']})\n';	
+	var code = 'ATP.get_data("aladdin.anser", **{"pricer": {"asset_id": ' + string_input + ', "Price": ' + string3_input + '}, "metrics": [' + string2_input + ']})\n';	
   }
   else if (loadInput=="FROM_PRT") {
 	string4_input = "'" + string4_input + "'";
-	var code = 'ATP.get_data("aladdin.riskfactors", **{"port":' + string_input + ', "rpt": ' + string2_input + ', "rpt_format": ' + string3_input + ', "date": ' + string4_input + ', "days": ' + string5_input + ', "schema": ' + string6_input + ', "rpt_data_col_name_key": ' + string7_input + '})\n';
+	var code = 'ATP.get_data("aladdin.riskfactors", **{"port":' + string_input + ', "rpt": ' + string2_input + ', "rpt_format": ' + string3_input + ', "date": ' + string4_input + ', "days": ' + string5_input + ', "schema": ' + string6_input + ', "rpt_data_col_name_key": ' + string7_input + '})\n';	
   }
   else {
 	var code = 'print("Test")\n';	
@@ -590,8 +639,12 @@ Blockly.Python['visualize_main'] = function(block) {
 	var col_input = Blockly.Python.valueToCode(block, 'LOAD_COL', Blockly.Python.ORDER_ATOMIC);
 	var col2_input = Blockly.Python.valueToCode(block, 'LOAD_COL2', Blockly.Python.ORDER_ATOMIC);	
 	
-	var loadInput = this.getFieldValue('PROPERTY'); 
-	
+	var loadInput = this.getFieldValue('PROPERTY');
+
+	string_input = string_input.replace(/[']/g,""); //Remove quotes from string
+	col_input = col_input.replace(/[']/g,""); //Remove quotes from string
+	col2_input = col2_input.replace(/[']/g,""); //Remove quotes from string
+			
 	if(loadInput=="VIZ_HEAD") {
 		var code = string_input + '.head()\n';
 	}
@@ -623,8 +676,35 @@ Blockly.Python['visualize_main'] = function(block) {
 	return [code, Blockly.Python.ORDER_ATOMIC];
 };
 
+Blockly.Python['transform_main'] = function(block) {
+	var string_input = Blockly.Python.valueToCode(block, 'LOAD_STRING', Blockly.Python.ORDER_ATOMIC);
+	var col_input = Blockly.Python.valueToCode(block, 'LOAD_COL', Blockly.Python.ORDER_ATOMIC);
+	var condition_input = Blockly.Python.valueToCode(block, 'LOAD_CONDITION', Blockly.Python.ORDER_ATOMIC);	
+	
+	var loadInput = this.getFieldValue('PROPERTY'); 
+	
+	string_input = string_input.replace(/[']/g,""); //Remove quotes from string
+	col_input = col_input.replace(/[']/g,""); //Remove quotes from string
+	condition_input = condition_input.replace(/[']/g,""); //Remove quotes from string
+	
+	if(loadInput=="TRANSFORM_CONCAT") {
+		//Assume concatenation by index. Can be enhanced to take in a column name if required
+		var code = 'pd.merge(' + string_input + ',' + col_input + ', how="inner", left_on=None, right_on=None, left_index=True, right_index=True, sort=True, suffixes=("_x", "_y"), copy=True, indicator=False)\n';
+	}
+	else if(loadInput=="TRANSFORM_INDEX") {
+		var code = string_input + '.set_index(' + col_input + ')\n';
+	}
+	else
+	{
+		var code = 'print("Test")\n';
+	}
+	return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+
 Blockly.Python['fit_main'] = function(block) {
 	var loadInput = this.getFieldValue('FIT_TYPE');
+	var returnType = this.getFieldValue('RETURN_TYPE');
 	var string_input = Blockly.Python.valueToCode(block, 'LOAD_STRING', Blockly.Python.ORDER_ATOMIC);
 	var dep_input = Blockly.Python.valueToCode(block, 'LOAD_DEPVAR', Blockly.Python.ORDER_ATOMIC);
 	var ratio_input = Blockly.Python.valueToCode(block, 'LOAD_RATIO', Blockly.Python.ORDER_ATOMIC);	
@@ -634,42 +714,44 @@ Blockly.Python['fit_main'] = function(block) {
 	indep_input = indep_input.replace(/[']/g,""); //Remove quotes from string	
 	
 	if(loadInput=="FIT_REGRESSION"){
-		var code = 'mld = MLData()\n';
+		var code = 'mld = MLData()\n'; //Rewrite simple regression code. Need flexible reference to columns/rows in dataframes.
 		code += 'mlm = MLModel()\n';
-		code += 'mld.data=' + string_input + '\n';
+		code += 'mld.data=' + string_input + '\n';		
 		code += 'features = ["' + indep_input + '"]\n';
 		code += 'mld.create_dummy_data(features)\n';
-		code += 'X_train, y_train, X_test, y_test = mlm.split_random(mld.dummies, mld.data["' + dep_input + '"],train_ratio=' + ratio_input + ' )\n';
+		code += 'X_train, y_train, X_test, y_test = mlm.split_random(mld.dummies, mld.data["' + dep_input + '"],train_ratio=1)\n';
 		code += 'mlm.define_regressor(reg_name="ols")\n';
 		code += 'mlm.fit_model(X_train, y_train, indep_cols=features)\n';
 		code += '\n';
-		code += 'print("R-Squared On Training Set")\n';
-		code += 'pred_train = mlm.predict(X_train)\n';
-		code += 'scores = mlm.eval_model(y_train, pred_train, metric="r2")\n';
-		code += 'print(scores.round(4))\n';
-		code += 'print("R-Squared On Testing Set")\n';
-		code += 'pred_test = mlm.predict(X_test)\n';
-		code += 'scores = mlm.eval_model(y_test, pred_test, metric="r2")\n';
-		code += 'print(scores.round(4))\n';
-		code += 'print("Model Estimated")\n';
-		code += 'coef = pd.DataFrame({"Indep_Var": mlm.dummy_indep_cols, "Coef": mlm.model.coef_})\n';
-		code += 'print(coef)\n';
-		code += '#This section for chart\n';
-		code += 'predictions = pd.concat([pred_test, y_test, mld.data.ix[y_test.index, mlm.indep_cols]], axis=1)\n';
-		code += 'trace1=go.Scatter(\n';
-		code += 'y=predictions["Predicted ' + dep_input + '"],\n';
-		code += 'x=predictions["' + indep_input + '"],\n';
-		code += 'mode="markers",\n';
-		code += 'marker = dict(color="rgba(255, 0, 0, .8)")\n';
-		code += ')\n';
-		code += 'trace2=go.Scatter(\n';
-		code += 'y=predictions["' + dep_input + '"],\n';
-		code += 'x=predictions["' + indep_input + '"],\n';
-		code += 'mode="markers",\n';
-		code += 'marker = dict(color="rgba(0, 0, 0, .8)")\n';
-		code += ')\n';
-		code += 'data=[trace1,trace2]\n';
-		code += 'plotly.offline.iplot(data, filename="styled-scatter")\n';
+		if(returnType=="RETURN_COEF") {
+			code += 'print(' + dep_input + ', mlm.model.coef_)\n';		
+		}
+		else if(returnType=="RETURN_R2") {
+			code += 'pred_train = mlm.predict(X_train)\n';
+			code += 'scores = mlm.eval_model(y_train, pred_train, metric="r2")\n';
+			code += 'print(' + dep_input + ', scores.round(4))\n';
+		}
+		else if(returnType=="RETURN_PLOT") {
+			code += 'pred_train = mlm.predict(X_train)\n';
+			code += 'predictions = pd.concat([pred_train, y_train, mld.data.ix[y_train.index, mlm.indep_cols]], axis=1)\n';
+			code += 'trace1=go.Scatter(\n';
+			code += 'y=predictions["Predicted " + ' + dep_input + '],\n';
+			code += 'x=predictions[' + indep_input + '],\n';
+			code += 'mode="markers",\n';
+			code += 'marker = dict(color="rgba(255, 0, 0, .8)")\n';
+			code += ')\n';
+			code += 'trace2=go.Scatter(\n';
+			code += 'y=predictions[' + dep_input + '],\n';
+			code += 'x=predictions[' + indep_input + '],\n';
+			code += 'mode="markers",\n';
+			code += 'marker = dict(color="rgba(0, 0, 0, .8)")\n';
+			code += ')\n';
+			code += 'data=[trace1,trace2]\n';
+			code += 'plotly.offline.iplot(data, filename="styled-scatter")\n';
+		}
+		else {
+			code += 'print("Test")\n';
+		}
 	}
 	else if (loadInput=="FIT_FOREST"){
 		var code = 'mld = MLData()\n';
@@ -712,5 +794,6 @@ Blockly.Python['fit_main'] = function(block) {
 	else {
 		var code = 'print("Test")\n';
 	}
-	return [code, Blockly.Python.ORDER_ATOMIC];
+	
+	return code; //Return plain text for use in Logic blocks
 };
